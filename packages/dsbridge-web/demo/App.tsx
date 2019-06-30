9474dfa37efc7d74f403f03253e4122729855da2
HU Ze <huze@outlook.sg>
2019-06-30 15:39:02 +1200

    web: make demo simpler

--- packages/dsbridge-web/demo/App.tsx
+++ packages/dsbridge-web/demo/App.tsx
@@ -1,13 +1,15 @@
+// Load dsbridge-web.preload
 import '../dist/preload';
+// Load wallet.core
 import './wallet.core';
-
+// Load dsbridge-web library
 import { callHandler } from '../dist';
 
+// Call sync function
 const sum = callHandler('wallet-core.add', [1, 20]);
 console.log('sum', sum);
 
+// call async function
 callHandler('wallet-core.add-asyn', [10, 2], (sum: number) => {
   console.log('asyn sum', sum);
 });
-
-export const App = () => null;
