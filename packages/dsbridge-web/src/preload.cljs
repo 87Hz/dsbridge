d5a528c9b5458b66391c0c72b5532b070e393483
HU Ze <huze@outlook.sg>
2019-06-30 15:20:54 +1200

    web: dsf and dsaf should not be the same ref

--- packages/dsbridge-web/src/preload.cljs
+++ packages/dsbridge-web/src/preload.cljs
@@ -2,15 +2,19 @@
 
 (defn call [] nil)
 
+(defn init-dsfs
+  "Construct DSBridge function registry"
+  []
+  #js {:_obs #js []})
+
 (defn init
   "Init Web to be DSBridge ready"
   []
   (let [global js/window
-        js-methods #js {:_obs #js []}
         dsbridge #js {:call call}]
     (aset global "dscb" 0)
     (aset global "_dsInit" false)
-    (aset global "_dsf" js-methods)
-    (aset global "_dsaf" js-methods)
+    (aset global "_dsf" (init-dsfs))
+    (aset global "_dsaf" (init-dsfs))
     (aset global "_dsbridge" dsbridge)
     (prn "DSBridge for Web initialized.")))
