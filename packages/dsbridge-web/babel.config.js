378f7d86151f62eff7fdbe105ecc1de7a109262a
HU Ze <huze@outlook.sg>
2019-07-05 17:43:14 +1200

    web: use babel-plugin-transform-imports

--- packages/dsbridge-web/babel.config.js
+++ packages/dsbridge-web/babel.config.js
@@ -1,4 +1,22 @@
 module.exports = {
   presets: ['@babel/preset-typescript', '@babel/preset-env'],
-  plugins: ['ramda'],
+  plugins: [
+    [
+      'transform-imports',
+      {
+        ramda: {
+          transform: 'ramda/src/${member}',
+          preventFullImport: true,
+        },
+        rxjs: {
+          transform: 'rxjs/${member}',
+          preventFullImport: true,
+        },
+        'rxjs/operators': {
+          transform: 'rxjs/operators/${member}',
+          preventFullImport: true,
+        },
+      },
+    ],
+  ],
 };
