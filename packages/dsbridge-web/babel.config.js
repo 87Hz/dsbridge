a9425d7d9448e5d969d8b28b2fe8c5da979e6077
HU Ze <huze@outlook.sg>
2019-07-06 16:47:30 +1200

    web: do not transform rxjs imports

--- packages/dsbridge-web/babel.config.js
+++ packages/dsbridge-web/babel.config.js
@@ -8,14 +8,6 @@ module.exports = {
           transform: 'ramda/src/${member}',
           preventFullImport: true,
         },
-        rxjs: {
-          transform: 'rxjs/${member}',
-          preventFullImport: true,
-        },
-        'rxjs/operators': {
-          transform: 'rxjs/operators/${member}',
-          preventFullImport: true,
-        },
       },
     ],
   ],
