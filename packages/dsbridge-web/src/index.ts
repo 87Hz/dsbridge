566d9b5c5d4fb0bb8e984fa625cd33208383ff16
HU Ze <huze@outlook.sg>
2019-07-08 00:09:40 +1200

    web: refined to v2

--- packages/dsbridge-web/src/index.ts
+++ packages/dsbridge-web/src/index.ts
@@ -1,30 +1,81 @@
 import { Subject } from 'rxjs';
 import { first } from 'rxjs/operators';
-import { pathOr, F, T, propEq, startsWith, has } from 'ramda';
+import { pathOr, T, F, startsWith, has, always, cond, equals } from 'ramda';
 
-interface WebReturnType {
-  id: number;
-  complete: string;
-  data: any;
-}
-
-const global: any = window;
 let nextCallbackId = 0;
-const returnValSub = new Subject<WebReturnType>();
+const global: any = window;
+const returnValSub = new Subject<string>();
 
 /** ---------------------------------------------------------
  * nativeHandlers
  */
+export type NativeSyncHandler = (argsJson: string) => string | void;
+export type NativeAsyncHandler = (argsJson: string) => Promise<string | void>;
+
 const isBuiltinMethodName = startsWith('_dsb.');
-const nativeHandlers: Record<string, any> = {};
+const nativeSyncHandlers: Record<string, NativeSyncHandler> = {};
+const nativeAsyncHandlers: Record<string, NativeAsyncHandler> = {};
 
 /** ---------------------------------------------------------
  * init
  */
+export interface HandlerRes {
+  data: any;
+  error?: HandlerError;
+}
+
+export interface HandlerError {
+  code: string;
+  value: string;
+}
+
+const createErrorHandlerSyncFn = (
+  code: string,
+  value: string
+): NativeSyncHandler => () => {
+  const res: HandlerRes = { data: null, error: { code, value } };
+  return JSON.stringify(res);
+};
+
+const createErrorHandlerAsyncFn = (
+  code: string,
+  value: string
+): NativeAsyncHandler => async () => {
+  const res: HandlerRes = { data: null, error: { code, value } };
+  return JSON.stringify(res);
+};
+
 (() => {
-  global._dsbridge = {
-    call: (method: string, arg: string) =>
-      pathOr<Function>(F, [method], nativeHandlers)(arg),
+  global['_dsbridge'] = {
+    call: (method: string, argsJson: string) => {
+      const { _dscbstub } = JSON.parse(argsJson);
+
+      // ----------------------------------------------------
+      // sync
+      if (!_dscbstub) {
+        const nativeSyncHandler: NativeSyncHandler = pathOr(
+          createErrorHandlerSyncFn('native-sync-handler-not-found', method),
+          [method],
+          nativeSyncHandlers
+        );
+        return nativeSyncHandler(argsJson);
+      }
+
+      // ----------------------------------------------------
+      // async
+      (async () => {
+        const nativeAsyncHandler: NativeAsyncHandler = pathOr(
+          createErrorHandlerAsyncFn('native-async-handler-not-found', method),
+          [method],
+          nativeAsyncHandlers
+        );
+        const ret = await nativeAsyncHandler(argsJson);
+        const callbackFn: Function = pathOr(F, [_dscbstub], global);
+        callbackFn(ret);
+      })();
+
+      return;
+    },
   };
 })();
 
@@ -40,10 +91,14 @@ export const hasJavascriptMethod: (method: string) => boolean = pathOr(
 /** ---------------------------------------------------------
  * callHandler
  */
-export const callHandler = <T = any>(
-  method: string,
-  args: any[] = []
-): Promise<T> => {
+const idMatched = (currId: number) => (argsJson: string) => {
+  const {
+    data: { id },
+  } = JSON.parse(argsJson);
+  return id === currId;
+};
+
+export const callHandler = (method: string, args: any = {}) => {
   const id = nextCallbackId++;
 
   const infoJson = {
@@ -53,33 +108,33 @@ export const callHandler = <T = any>(
   };
 
   return new Promise((resolve) => {
-    returnValSub
-      .pipe<WebReturnType>(first(propEq('id', id)))
-      .subscribe((ret) => resolve(ret.data));
+    returnValSub.pipe(first(idMatched(id))).subscribe(resolve);
     global._handleMessageFromNative(infoJson);
   });
 };
 
 /** ---------------------------------------------------------
- * addNativeMethod
+ * addNativeSyncMethod
  */
-export const addNativeMethod = <T = any, R = any>(
+export const addNativeSyncMethod = (
   method: string,
-  func: (args: T) => R | Promise<R>,
+  func: NativeSyncHandler,
   allowOverwriteBuiltin: boolean = false
 ) => {
   if (!allowOverwriteBuiltin && isBuiltinMethodName(method)) return;
+  nativeSyncHandlers[method] = func;
+};
 
-  nativeHandlers[method] = (json: string): any => {
-    const { data: args, _dscbstub } = JSON.parse(json);
-
-    // sync
-    if (!_dscbstub) return JSON.stringify({ data: func(args) });
-
-    // async
-    (async () =>
-      global[_dscbstub](JSON.stringify({ data: await func(args) })))();
-  };
+/** ---------------------------------------------------------
+ * addNativeAsyncMethod
+ */
+export const addNativeAsyncMethod = (
+  method: string,
+  func: NativeAsyncHandler,
+  allowOverwriteBuiltin: boolean = false
+) => {
+  if (!allowOverwriteBuiltin && isBuiltinMethodName(method)) return;
+  nativeAsyncHandlers[method] = func;
 };
 
 /** ---------------------------------------------------------
@@ -87,28 +142,62 @@ export const addNativeMethod = <T = any, R = any>(
  */
 export const removeNativeMethod = (method: string) => {
   if (isBuiltinMethodName(method)) return;
-  delete nativeHandlers[method];
+
+  delete nativeSyncHandlers[method];
+  delete nativeAsyncHandlers[method];
 };
 
 /** ---------------------------------------------------------
  * builtinMethods
  */
-addNativeMethod('_dsb.dsinit', T, true);
-addNativeMethod('_dsb.disableJavascriptDialogBlock', T, true);
-
-addNativeMethod(
-  '_dsb.returnValue',
-  (arg) => {
-    returnValSub.next(arg);
+addNativeSyncMethod(
+  '_dsb.dsinit',
+  () => {
+    const res: HandlerRes = { data: true };
+    return JSON.stringify(res);
   },
   true
 );
 
-addNativeMethod(
+addNativeSyncMethod(
+  '_dsb.disableJavascriptDialogBlock',
+  (argsJson) => {
+    const { disable } = JSON.parse(argsJson);
+    const res: HandlerRes = { data: disable };
+    return JSON.stringify(res);
+  },
+  true
+);
+
+addNativeSyncMethod(
+  '_dsb.returnValue',
+  (argsJson) => {
+    returnValSub.next(argsJson);
+    return;
+  },
+  true
+);
+
+addNativeSyncMethod(
   '_dsb.hasNativeMethod',
-  (args) => {
-    const name = pathOr('UnknownFunc', ['name'], args);
-    return has(name, nativeHandlers);
+  (argsJson) => {
+    const {
+      data: { name, type },
+    } = JSON.parse(argsJson);
+
+    const hasSyncMethod = has(name, nativeSyncHandlers);
+    const hasAsyncMethod = has(name, nativeAsyncHandlers);
+    const hasAllMethods = hasSyncMethod && hasAsyncMethod;
+
+    const exists = cond([
+      [equals('syn'), always(hasSyncMethod)],
+      [equals('asyn'), always(hasAsyncMethod)],
+      [equals('all'), always(hasAllMethods)],
+      [T, F],
+    ])(type);
+
+    const res: HandlerRes = { data: exists };
+    return JSON.stringify(res);
   },
   true
 );
